6fe6fcb79d93e9a34a3e80a6e569c8ab98a9124f
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2025-03-17 18:16:55 -0300

    Update not_shutdown_for_windows.py

--- not_shutdown_for_windows.py
+++ not_shutdown_for_windows.py
@@ -3,7 +3,7 @@
 
 # not_shutdown.py
 
-# O script not_shutdown.py utiliza a biblioteca ctypes para fazer uma chamada à API do Windows e prevenir que o computador entre em modo de suspensão automática.
+# O script not_shutdown_for_windows.py utiliza a biblioteca ctypes para fazer uma chamada à API do Windows e prevenir que o computador entre em modo de suspensão automática.
 
 import ctypes
 import time
