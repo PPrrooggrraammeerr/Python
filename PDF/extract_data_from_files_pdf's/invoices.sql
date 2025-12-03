5747274267514e522f0eeea68dafe326e3a7186a
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2025-12-02 21:39:04 -0300

    Update invoices.sql

--- PDF/extract_data_from_files_pdf's/invoices.sql
+++ PDF/extract_data_from_files_pdf's/invoices.sql
@@ -7,7 +7,8 @@ USE invoices;
 CREATE TABLE invoices (
     id SERIAL PRIMARY KEY,
     invoice_number VARCHAR(50) NOT NULL,
-    invoice_date DATE NOT NULL,
+    invoice_date VARCHAR(50) NOT NULL,
     file_name VARCHAR(255),
     status VARCHAR(50)
 );
+
